0c2d8e21b2f80db84943ba7857859188039a1317
ghdgu <ghdguswl13@gmail.com>
2022-06-01 21:52:13 +0900

    주석 추가

--- app/src/main/java/com/primitive_app/posttogether/MainActivity.java
+++ app/src/main/java/com/primitive_app/posttogether/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//노트북2 작성
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
